42bc4f56493938722f66109f01ab7f62d0ee3361
DukloRen <benedekbarany@gmail.com>
2024-02-29 14:16:14 +0100

    Users table is almost fully functional, but still needs designing.

--- View/UsersView.xaml.cs
+++ View/UsersView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data;
 using System.Linq;
 using System.Text;
@@ -25,7 +26,30 @@ namespace MonkeSwap_Desktop.View
         public UsersView()
         {
             InitializeComponent();
-            string connectionString = "SERVER=localhost;DATABASE=testdb;UID=root;PASSWORD=;";
+
+            var converter = new BrushConverter();
+            ObservableCollection<User> users = new ObservableCollection<User>();
+
+            //Create DataGrid Items
+            users.Add(new User { Username= "Kóth Tevin", Email = "kt@gmail.com", TC = 9, DOR = "25/02/2024", Role = "admin" });
+            users.Add(new User { Username = "Kóth Tevin2", Email = "kt@gmail.hu", TC = 11, DOR = "24/02/2024", Role = "user" });
+            users.Add(new User { Username = "Kóth Tevin3", Email = "kt@citromail.hu", TC = 20, DOR = "29/02/2024", Role = "user" });
+            users.Add(new User { Username= "Kóth Tevin4", Email = "kt@email.com", TC = 13, DOR = "28/02/2024", Role = "user" });
+            users.Add(new User { Username= "Kóth Tevin5", Email = "kt@email.hu", TC = 20, DOR = "26/02/2024", Role = "user" });
+            users.Add(new User { Username = "Kóth Tevin", Email = "kt@gmail.com", TC = 9, DOR = "25/02/2024", Role = "admin" });
+            users.Add(new User { Username = "Kóth Tevin2", Email = "kt@gmail.hu", TC = 11, DOR = "24/02/2024", Role = "user" });
+            users.Add(new User { Username = "Kóth Tevin3", Email = "kt@citromail.hu", TC = 20, DOR = "29/02/2024", Role = "user" });
+            users.Add(new User { Username = "Kóth Tevin4", Email = "kt@email.com", TC = 13, DOR = "28/02/2024", Role = "user" });
+            users.Add(new User { Username = "Kóth Tevin5", Email = "kt@email.hu", TC = 20, DOR = "26/02/2024", Role = "user" });
+            users.Add(new User { Username = "Kóth Tevin", Email = "kt@gmail.com", TC = 9, DOR = "25/02/2024", Role = "admin" });
+            users.Add(new User { Username = "Kóth Tevin2", Email = "kt@gmail.hu", TC = 11, DOR = "24/02/2024", Role = "user" });
+            users.Add(new User { Username = "Kóth Tevin3", Email = "kt@citromail.hu", TC = 20, DOR = "29/02/2024", Role = "user" });
+            users.Add(new User { Username = "Kóth Tevin4", Email = "kt@email.com", TC = 13, DOR = "28/02/2024", Role = "user" });
+            users.Add(new User { Username = "Kóth Tevin5", Email = "kt@email.hu", TC = 20, DOR = "26/02/2024", Role = "user" });
+
+            dtGrid.ItemsSource = users;
+            
+            /*string connectionString = "SERVER=localhost;DATABASE=testdb;UID=root;PASSWORD=;";
 
             MySqlConnection connection = new MySqlConnection(connectionString);
 
@@ -36,7 +60,16 @@ namespace MonkeSwap_Desktop.View
             dt.Load(cmd.ExecuteReader());
             connection.Close();
 
-            dtGrid.DataContext = dt;
+            dtGrid.DataContext = dt;*/
         }
+    }
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public int TC { get; set; }
+        public string DOR { get; set; }
+        public string Role { get; set; }
     }
 }
